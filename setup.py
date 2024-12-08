ebe846859a38c70c97b14401f00d7442886c64d3
Maria Helena Larroza <malenaa2905@gmail.com>
2024-12-08 19:29:22 -0300

    Atualizações

--- setup.py
+++ setup.py
@@ -2,4 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="financas",
-    version
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    tests_require=["pytest"],
+)
